dc03740fb3402211c379f21d2c518a4de86a5e1d
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-02-02 06:09:10 +0300

    feat: updated

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,5 @@
 import { Button, Flex, Text } from "@chakra-ui/react";
 import Image from "next/image";
-import React from "react";
 import image404 from "../public/404.jpg";
 import { useRouter } from "next/router";
 const Custom404 = () => {
@@ -14,7 +13,7 @@ const Custom404 = () => {
       alignItems="center"
       minWidth="100%"
     >
-      <Image src={image404} height={600} width={600} alt="404" an/>
+      <Image src={image404} height={600} width={600} alt="404" />
       <Text fontSize="4xl" my="3">
         Bu sayfa bulunamadı
       </Text>
